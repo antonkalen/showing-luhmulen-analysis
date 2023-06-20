9e2c4f727208f2927020d0169de94269b773966c
Anton Kalén <anton.kalen@gmail.com>
2023-06-20 21:26:16 +0200

    Initial commit

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -12,13 +12,11 @@ library(stringr)
 
 riders <- tribble(
   ~rider, ~horse, ~horse_id, ~final_time,
-  "Aminda Ingulfson", "Joystick", "joystick", 425
+  "Aminda Ingulfson", "Joystick", "joystick", 425,
+  "Sofia Sjöborg", "Bryjamolga van het Marienshof Z", "belle", 386,
+  "Sofia Sjöborg", "Targa D", "targa", 434
 )
 
-jump_ids <- c("1", "2", "3", "4", "5a", "5b", "5c", "6a", "6b", "7", "8a", "8b",
-              "8c", "9", "10a", "10b", "10c", "11", "12a", "12b", "13", "14a",
-              "14b", "14c", "15", "16", "17a", "17b", "18a", "18b", "18c", "19",
-              "20a", "20b", "21")
 
 jumps_raw <- dir_ls(here("data-raw"), glob = "*jumps*") |>
   read_delim(
@@ -38,10 +36,9 @@ jump_clean <- jumps_raw |>
   mutate(
     horse_id = str_extract(basename(horse_id), "(.*)_(.*)_(.*)", group = 2),
     across(
-      c(lateral_balance, straightness, longitudinal_balance),
+      c(lateral_balance, longitudinal_balance),
       \(x) as.numeric(str_extract(x, "[\\-\\.0-9]*"))
-    ),
-    lateral_balance2 = lateral_balance,
+    )
   )
 
 strides_clean <- strides_raw |>
@@ -49,12 +46,12 @@ strides_clean <- strides_raw |>
     horse_id = str_extract(basename(horse_id), "(.*)_(.*)_(.*)", group = 2),
     across(
       c(duration, strike_power, speed, max_height, length, lateral_balance,
-        straightness, longitudinal_balance),
+        longitudinal_balance),
       \(x) as.numeric(str_extract(x, "[\\-\\.0-9]*"))
     ),
     max_height = max_height / 100,
     across(
-      c(strike_power, lateral_balance, straightness, longitudinal_balance),
+      c(strike_power, lateral_balance, longitudinal_balance),
       list(min = ~.x - 0.5, max = ~.x + 0.5)
     )
   )
@@ -73,7 +70,6 @@ jump_added_info <- jump_clean |>
       between(strike_power_g, strike_power_min, strike_power_max),
       avg_speed_m_min == speed,
       between(lateral_balance, lateral_balance_min, lateral_balance_max),
-      between(straightness, straightness_min, straightness_max),
       between(longitudinal_balance, longitudinal_balance_min, longitudinal_balance_max),
     ),
     relationship = "many-to-one",
@@ -101,8 +97,6 @@ jump_calc_time <- jump_added_info |>
 # Add jump info
 jump_filtered <- jump_calc_time |> filter(type == "Jump")
 
-jump_filtered$jump_id <- rep(jump_ids, times = n_distinct(jump_calc_time$horse_id))
-
 jump_selected <- jump_filtered |>
   select(
     horse_id,
@@ -113,7 +107,6 @@ jump_selected <- jump_filtered |>
     max_height = jump_height_m,
     length = length_m,
     lateral_balance = lateral_balance.x,
-    straightness = straightness.x,
     max_take_off_angle,
     jump_id
   )
@@ -150,7 +143,6 @@ full_selected_data <- full_filtered_data |>
     hand,
     lateral_balance,
     longitudinal_balance,
-    straightness,
     jump_id
   )
 
